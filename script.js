2c3373dee778ea5e2442a9b4c167f092f2bb9159
Dikesh Kumar <117905520+Dikesh01@users.noreply.github.com>
2023-06-24 16:08:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const name = document.getElementById('name');
+const age= document.getElementById('age');
+const button = document.getElementById('btn');
+
+button.addEventListener('click', function(event){
+  event.preventDefault();
+	let nameValue = name.value;
+	let ageValue = age.value;
+  if(!(nameValue && ageValue)){
+    alert("inputs cannot be empty.");
+    return;
+  }
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      if(ageValue > 18){
+        alert(`Welcome, ${nameValue} You can vote.`)
+      }
+      else{
+        alert(`Oh sorry ${nameValue} You aren't old enough.`)
+      }
+    },4000)
+  })
+  
+})
